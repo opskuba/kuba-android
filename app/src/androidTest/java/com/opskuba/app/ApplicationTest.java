d698add35be3e6b6a3c466880ce4bae9fe9a4f20
Albert Sun <atschx@gmail.com>
2015-06-24 00:45:20 +0800

    refactor

--- app/src/androidTest/java/com/opskuba/app/ApplicationTest.java
+++ app/src/androidTest/java/com/opskuba/app/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.albert.opskuba;
+package com.opskuba.app;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/opskuba/app/MainActivityFragment.java
+++ app/src/main/java/com/opskuba/app/MainActivityFragment.java
@@ -1,10 +1,11 @@
-package com.example.albert.opskuba;
+package com.opskuba.app;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 
 /**
@@ -15,9 +16,16 @@ public class MainActivityFragment extends Fragment {
     public MainActivityFragment() {
     }
 
+    /**
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
